283866dc34eb87adab945e588aed60e67afff927
theaapfiatina <theaapfa28@gmail.com>
2023-10-06 11:38:46 +0800

    tugasmobile

---
+++ bin/function_short_expression.dart
@@ -0,0 +1,5 @@
+void main() {
+  int sum(int first, int second) => first + second;
+  print(sum(10, 10));
+  print(sum(20, 20));
+}
---
+++ bin/higher_order_function.dart
@@ -0,0 +1,13 @@
+void main(List<String> arguments) {
+  final twicePlusFive = twice((x) => x + 5);
+  final result = twicePlusFive(3);
+  print(result);
+}
+
+typedef IntTransformer = int Function(int);
+
+IntTransformer twice(IntTransformer f) {
+  return (int x) {
+    return f(f(x));
+  };
+}
