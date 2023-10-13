c0dc33a367e53f3d6cd4813994c5679e3d51b92f
theaapfiatina <theaapfa28@gmail.com>
2023-10-13 20:24:29 +0800

    tugasmobile

---
+++ bin/OOP/MethodExpressionBody.dart
@@ -0,0 +1,13 @@
+class Computer {
+  void startup() => print("computer is starting");
+  void shutdown() => print('computer is shutting down');
+
+  String getOperatingSystem() => "Linux";
+}
+
+void main() {
+  var computer = Computer();
+  computer.startup();
+  computer.shutdown();
+  print(computer.getOperatingSystem());
+}
